7d18f80894de55ab0c310a49c3e2260c88f37a62
Viditv68 <viditrawatv68@gmail.com>
2020-08-20 19:55:43 +0530

    Main Menu Controller and Music Controller

---
+++ Assets/Scripts/MainMenuController/MainMenuController.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+
+    [SerializeField]
+    private Animator settingsButtonAnim;
+
+    private bool hidden;
+    private bool canTouchSettingsButton;
+
+    [SerializeField]
+    private Button musicButton;
+
+    [SerializeField]
+    private Sprite[] musicButtonSprite;
+
+    [SerializeField]
+    private Button fbButton;
+
+    [SerializeField]
+    private Sprite[] fbSprites;
+
+    [SerializeField]
+    private GameObject infoPanel;
+
+    [SerializeField]
+    private Image infoImage;
+
+    [SerializeField]
+    private Sprite[] infoSprites;
+
+    private int infoIndex;
+
+    void Start()
+    {
+        canTouchSettingsButton = true;
+        hidden = true;
+
+        if(GameController.instance.isMusicOn)
+        {
+            MusicController.instance.PlayBgMusic();
+            musicButton.image.sprite = musicButtonSprite[0];
+        }
+        else
+        {
+            MusicController.instance.StopBgMusic();
+            musicButton.image.sprite = musicButtonSprite[1];
+        }
+
+        infoIndex = 0;
+        infoImage.sprite = infoSprites[infoIndex];
+    }
+
+   
+    void Update()
+    {
+        
+    }
+
+
+    public void SettingsButton()
+    {
+        StartCoroutine(DissableSettingsButtonWhilePlayingAnimation());
+    }
+
+
+
+    public void MusicButton()
+    {
+        if(GameController.instance.isMusicOn)
+        {
+            musicButton.image.sprite = musicButtonSprite[1];
+            MusicController.instance.StopBgMusic();
+            GameController.instance.isMusicOn = false;
+            GameController.instance.Save();
+
+        }
+        else
+        {
+            musicButton.image.sprite = musicButtonSprite[0];
+            MusicController.instance.PlayBgMusic();
+            GameController.instance.isMusicOn = true;
+            GameController.instance.Save();
+        }
+    }
+
+
+    public void OpenInfoPanel()
+    {
+        infoPanel.SetActive(true);
+    }
+
+
+    public void CloseInfoPanel()
+    {
+        infoPanel.SetActive(false);
+    }
+
+    public void NextInfo()
+    {
+        infoIndex++;
+
+        if(infoIndex == infoSprites.Length)
+        {
+            infoIndex = 0;
+        }
+
+        infoImage.sprite = infoSprites[infoIndex];
+    }
+
+
+    public void PlayButton()
+    {
+        MusicController.instance.PlayClickClip();
+    }
+
+    IEnumerator DissableSettingsButtonWhilePlayingAnimation()
+    {
+        if(canTouchSettingsButton)
+        {
+            if(hidden)
+            {
+                canTouchSettingsButton = false;
+                settingsButtonAnim.Play("SlideIn");
+                hidden = false;
+                yield return new WaitForSeconds(1.2f);
+                canTouchSettingsButton = true;
+            }
+
+            else
+            {
+                canTouchSettingsButton = false;
+                settingsButtonAnim.Play("SlideOut");
+                hidden = true;
+                yield return new WaitForSeconds(1.2f);
+                canTouchSettingsButton = true;
+            }
+        }
+    }
+
+
+
+}
---
+++ Assets/Scripts/MusicController/MusicController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicController : MonoBehaviour
+{
+
+    public static MusicController instance;
+
+    private AudioSource bgMusic, click;
+    private float time;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        MakeSingleton();
+
+        AudioSource[] audioSources = GetComponents<AudioSource>();
+
+        bgMusic = audioSources[0];
+        click = audioSources[1];
+    }
+
+
+    void MakeSingleton()
+    {
+        if(instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    
+    private void OnLevelWasLoaded()
+    {
+        if(SceneManager.GetActiveScene().name == "LevelMenu")
+        {
+            if (GameController.instance.isMusicOn)
+            {
+                if (!bgMusic.isPlaying)
+                {
+                    bgMusic.time = time;
+                    bgMusic.Play();
+                }
+            }
+        }
+    }
+    public void GameIsLoadedTurnOffMusic()
+    {
+        if (bgMusic.isPlaying)
+        {
+            time = bgMusic.time;
+            bgMusic.Stop();
+        }
+    }
+
+    
+    public void PlayBgMusic()
+    {
+        if(!bgMusic.isPlaying)
+        {
+            bgMusic.Play();
+        }
+    }
+
+    public void StopBgMusic()
+    {
+        if (bgMusic.isPlaying)
+        {
+            bgMusic.Stop();
+        }
+    }
+
+    public void PlayClickClip()
+    {
+        click.Play();
+    }
+}
